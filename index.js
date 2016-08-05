610672c2f6706c61ac889430887a326977b01e46
John Mann <jmann25@gmail.com>
2016-08-05 09:31:22 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,3 @@
+console.error("Incorrect PIN, try again or cancel transaction! Three incorrect tries will lock your card!")
+console.log("Incorrect PIN entered.")
+console.warn("Incorrect PIN, try again or cancel transaction! Three incorrect tries will lock your card!")
