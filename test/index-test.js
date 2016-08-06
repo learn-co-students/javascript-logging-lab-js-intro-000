c766a4508a224b071b2a4a68d2074e08c4f27be7
boon lim <limboontat@gmail.com>
2016-08-06 12:34:14 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,3 +1,9 @@
+const expect = require('expect')
+const fs = require('fs')
+const jsdom = require('jsdom')
+const path = require('path')
+
+
 describe('index', () => {
   const html = '<div></div>'
   const src = path.resolve(__dirname, '..', 'index.js')
@@ -24,4 +30,17 @@ describe('index', () => {
       console.log.restore()
       done()
     })
-  })     
+  })
+
+  it('calls console.warn()', done => {
+    const spy = expect.spyOn(console, 'warn').andCallThrough()
+
+    jsdom.env(html, [src], {
+      virtualConsole: jsdom.createVirtualConsole().sendTo(console)
+    }, (err, window) => {
+      expect(spy).toHaveBeenCalled('expected console.warn to have been called')
+      console.warn.restore()
+      done()
+    })
+  })
+})
