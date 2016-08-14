761cda52ee48aa831450a4b8eee97a5301c7d057
Peter Chuang <p.chuang89@gmail.com>
2016-08-14 04:28:29 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -39,6 +39,8 @@ describe('index', () => {
   it('calls console.warn()', done => {
     const spy = expect.spyOn(console, 'warn').andCallThrough()
 
+    console.warn("HALP!")
+
 
     jsdom.env(html, [src], {
       virtualConsole: jsdom.createVirtualConsole().sendTo(console)
