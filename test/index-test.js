a96ee8ed66e93d1ca4df48115955712541a7b83a
Micah Fletcher <kpandax@live.com>
2016-08-26 04:05:46 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -20,7 +20,7 @@ describe('index', () => {
     })
   })
 
-  it('calls console.log("I would be a logger.")', done => {
+  it('calls console.log("Logging!")', done => {
     const spy = expect.spyOn(console, 'log').andCallThrough()
 
     jsdom.env(html, [src], {
@@ -32,7 +32,7 @@ describe('index', () => {
     })
   })
 
-  it('calls console.warn("BROKEN!")', done => {
+  it('calls console.warn("Try again!")', done => {
     const spy = expect.spyOn(console, 'warn').andCallThrough()
 
     jsdom.env(html, [src], {
