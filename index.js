1dd2282c3616071ec8e1bddf0d18314486be4a60
Sagar Tambi <tambisagar@gmail.com>
2016-09-05 21:44:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-console.error("HALP!")
-index calls console.warn ()
-console.log("I would be a logger")//
+//declare the variable in the word
+var word
+assign a value to the variable
+word = 'bird'
+console.log(word) // 'bird'
+//assign another value to the website
+word = 'dog'
+console.log(word) // 'dog'
