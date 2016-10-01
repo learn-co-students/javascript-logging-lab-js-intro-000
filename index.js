f88360dc221b802b5efba620986fbb7a5e72376a
Sadhwika Chandramohan <sadhwika@yahoo.com>
2016-10-01 17:05:34 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log("Prompt user to swipe the card");
+console.error("Unable to read the card! Swipe again!");
+console.log("Ask for PIN");
+console.log("Ask for withdrawal amount");
+console.warn("Exceeding balance amount");
+console.error("Invalid amount");
