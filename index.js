ca8604b3f681a993a3e503570953489ddee051cf
Tara Gabsi <taragabsi@gmail.com>
2016-10-13 20:51:19 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,10 @@
+`learn test`
+console.error("Mommy!")
+`learn test
+`console.error("MOM")
+`console.error("please work")`
+`console.log('howdy')`
+`console.log("come on you can do it")`
+`console.warn("CAREFUL!)`
+console.log("I'm a logger")
+`console.warn("DANGER!)`
