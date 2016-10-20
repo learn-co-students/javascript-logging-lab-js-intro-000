55bb4d386aa2de469edc2692d10a820a04f6beb9
angie mc gauran <carmelmcg@optonline.net>
2016-10-20 18:06:40 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,3 +1,4 @@
+console.error("HALP")
 const expect = require('expect')
 const fs = require('fs')
 const jsdom = require('jsdom')
@@ -7,8 +8,7 @@ const path = require('path')
 describe('index', () => {
   const html = '<div></div>'
   const src = path.resolve(__dirname, '..', 'index.js')
-
-  it('calls console.error()', done => {
+it('calls console.error()', done => {
     const spy = expect.spyOn(console, 'error').andCallThrough()
 
     jsdom.env(html, [src], {
