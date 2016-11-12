45ab6fe94719d9898df77f9505ff1fc765ba7ef1
Clementina Reynolds <reynoldscf95@gmail.com>
2016-11-12 05:14:34 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,3 @@
+console.error("this is crazy, but you dog gone messed up somwhere")
+console.log("I think you completed your transaction")
+console.warn("you may have just transfered all your money to your mother-in-law.. but you also may not have. idfk I'm an ATM machine")
