c4d74b54499cb5aa7a99ffc92500e79f05e16c78
Adam Ergas <adamergas@yahoo.com>
2016-11-15 05:07:06 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,3 @@
+console.error('Something went wrong. Please contact an administrator.')
+console.log('Last updated: 11/14/16 23:04')
+console.warn('User input will result in an error. Please enter the appropriate data.')
