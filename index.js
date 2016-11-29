b57c8f6bb12a7120fcf461d9e4232ea8ac75a461
Yousuf Ali <yousufsemol@gmail.com>
2016-11-29 17:43:49 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,3 @@
+console.error("This will show the warn message!!!");
+console.log("This is string and print the message what written yet!");
+console.warn("This is warn message and I want it to be printed as warning.");
