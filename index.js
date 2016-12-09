b25f7360f30e1ba353dac012e23e2c05721739a3
Jessica Ibbotson <jessicaibb@gmail.com>
2016-12-09 04:29:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-console.error("help")
-console.error("HALP!")
-console.log("This isn't explained very well. Are there better Javascript books out there I can learn from?
-")
-console.warn("what the fuck?")
+console.error("help!")
+console.log("help!")
+console.warn("help!")
