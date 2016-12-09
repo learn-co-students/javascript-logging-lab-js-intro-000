145d3f3c075e5ca99fe51a6c540ce220360b0828
Jessica Ibbotson <jessicaibb@gmail.com>
2016-12-09 04:22:55 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -44,9 +44,3 @@ describe('index', () => {
     })
   })
 })
-console.log("help")
-console.error("help")
-console.error("HALP!")
-console.log("This isn't explained very well. Are there better Javascript books out there I can learn from?
-")
-console.warn("what the fuck?")
