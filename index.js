254f4ba7659a0c1ae9b23e02ef031de1f2647bc7
Kayvan Taherpour <kayvanote@gmail.com>
2017-01-19 22:21:03 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,3 @@
+console.log("Hey we have updated you to paperless setting. Thank you for being planet friendly!!!!")
+console.error("there was an error with the check deposited please call customer care at 1800-111-1111")
+console.warn('You should insert the bill facing up (presidents face can be seen)')
