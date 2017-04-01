9eee94243adb1a22d9337155cf4b0f08a3a6f8e4
Martine Stern <perri614@gmail.com>
2017-04-01 05:29:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-console.log("The user should not continue what they are doing!") // something is broken here, how can I fix it?
+console.log("The user should not continue what they are doing.") // we need to figure this out
+console.warn("Something is broken, but I do not know how to fix it.")
+console.error("HALP!")
