77ce4d8830f6eb4f98e3e5e5b773bab8d9df3094
Melissa Fisher-Brown <melissafisherbrown@gmail.com>
2017-04-11 07:24:36 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,5 @@
+console.error("HALP!")
+console.log("Consoling the ATM entries.");
+console.error("Your PIN is invalid.");
+console.warn("Finish making your selection.");
+console.error("Stop! Encountered an error. Please exit and try again.")
