b7d96ed224b5b465a67c447b05f96fc3db01650c
Suzen Fylke <sue@suzenfylke.com>
2017-04-11 20:49:48 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,3 @@
+console.error("This machine cannot dispense cash at this time.")
+console.log("Enter your pin number using the keypad.")
+console.warn("You may not remove your card during a transaction.")
