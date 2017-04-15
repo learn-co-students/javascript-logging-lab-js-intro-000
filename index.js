d92f8159a815552a97cd0442290158ed68384958
James Boydston <boydston.j@gmail.com>
2017-04-15 16:24:50 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,4 @@
+console.error ("I am here to HELP, do not worry!") //Gives notifacation problem on web browser
+console.log ("I'm a lumberjack, and I don't care. I work all day and sleep all night") //prints message to terminal
+console.warn ("I have never been so insulted in my life. A 'lumberjack,' are you for real?") //indicates potential issue 
+
