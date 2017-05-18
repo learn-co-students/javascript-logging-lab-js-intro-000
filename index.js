8fa88fadbec7c6b59d950ac173212beb29239584
Thomas Krill <tkrill@nd.edu>
2017-05-18 19:21:10 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,3 @@
+console.error("Gah! It was so close, too!")
+console.log("Saving your adventure...\nDon't turn off the power.")
+console.warn("Oak's words echoed...\nThere's a time and place for everything, but not now.")
