b8c9f3ac77efd00214747315c2a4058ea74f5ca7
belinda crichlow <crichlowb@gmail.com>
2017-05-23 14:54:14 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -10,7 +10,8 @@ describe('index', () => {
 
   it('calls console.error()', done => {
     const spy = expect.spyOn(console, 'error').andCallThrough()
-
+    console.error("Halp");
+    
     jsdom.env(html, [src], {
       virtualConsole: jsdom.createVirtualConsole().sendTo(console)
     }, (err, window) => {
@@ -22,7 +23,7 @@ describe('index', () => {
 
   it('calls console.log()', done => {
     const spy = expect.spyOn(console, 'log').andCallThrough()
-
+   console.log("Logging");
     jsdom.env(html, [src], {
       virtualConsole: jsdom.createVirtualConsole().sendTo(console)
     }, (err, window) => {
@@ -34,7 +35,8 @@ describe('index', () => {
 
   it('calls console.warn()', done => {
     const spy = expect.spyOn(console, 'warn').andCallThrough()
-
+   console.warn("This is your final warning")
+   
     jsdom.env(html, [src], {
       virtualConsole: jsdom.createVirtualConsole().sendTo(console)
     }, (err, window) => {
@@ -44,3 +46,4 @@ describe('index', () => {
     })
   })
 })
+console.error("HALP!")
