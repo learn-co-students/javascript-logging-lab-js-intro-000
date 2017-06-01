fed30a39ff3c2a0e36c811d276b46566b7a7fa84
Dan John <daniel.m.powell@students.jsums.edu>
2017-06-01 21:32:28 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,3 @@
+console.error("HALP!")
+console.warn("Beware the Man that Speaks in Hands")
+console.log("Specimen has been subjected to extreme levels of GUHoivndihsdnlINOUHNIN but seems stable.....interesting")
