4e4ad57e22ccaf8c1736a8c51a531031a1aaa1aa
Michael Yu <myu@cretona.com>
2017-06-14 13:54:42 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,5 @@
+console.error "HALP!") //We simply call console.error when "index calls console.error()"..
+console.log ("Bad_Doggo") //.. we go into the index.js file and we call for console.error (or the other ones)
+console.warn("RHAMCO") //what about objects?
+//0 = "HALP!"
+//Bad_Doggo ("Good Doggo")
