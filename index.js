f2082e8d063a9a698b447530f97e7da8ac878938
Veekas Shrivastava <veekas@veekasmeansprogress.com>
2017-06-24 19:53:45 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,3 @@
+console.error("This is the console error you're looking for.");
+console.log("This is the console log you\'re looking for.");
+console.warn("This the console warning you're looking for.");
