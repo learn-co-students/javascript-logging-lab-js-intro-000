61975e53b1df665c1897403de58e78e48abd2b63
Learn.co IDE <foo@example.com>
2017-07-26 03:07:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-console.log('one', 'two', 'three')
-console.error('Danger, Will Robinson!')
-console.warn('Hm, you might not want to do that.')
+console.log('opeopl', 'the', 'we')
+console.error('Dwer, Wdsf asdf!')
+console.warn('Hdfdfd, dsfs.')
