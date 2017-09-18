415738a9c48807ec39e870447509238c56731312
nxtevo <osanda@protonmail.com>
2017-09-18 11:05:59 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,8 @@
+// invoking console.error() function (logs errors to the console)
+console.error("HALP");
+
+//invoking console.log() function (logs information to the console)
+console.log("Your account balance is $1000");
+
+//invoking console.warn() function (logs warnings to the console)
+console.warn("Fatal Error!");
