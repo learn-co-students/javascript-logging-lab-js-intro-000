16cbab51867ecf25dbd99faa85aefc58be98f9de
Joe Lott <crowmands@gmail.com>
2018-01-04 04:12:29 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,3 @@
+console.error("Account number is not valid.  Please contact support.");
+console.log("Thank you for depositing moola!");
+console.warn("Your balance is not sufficient to withdraw that amount.");
