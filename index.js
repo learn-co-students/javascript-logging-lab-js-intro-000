9f1b213964795f00157828fbefcf502332e39750
alexvladstr@gmail.com <alexvladstr@gmail.com>
2018-05-11 19:22:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.warn("Hello world!")
+console.error("HALP!")
+<<<<<<< HEAD
+console.warn("I like JS!")
+=======
+console.log("I would be a logger.") // get it?
+>>>>>>> 857dbbe26db6d51178a38a8e978da028020d07e5
