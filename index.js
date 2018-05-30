92fe26ba71576b35cc2fa476ad33ae47976f69c8
Courtny Cotten <court@stackoverflow.com>
2018-05-30 14:53:58 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,3 @@
+console.error("HAIR IS ON FIRE! This is the first test! Let's do this thing!");
+console.log("LOG TEST! This is a console log, just rollin' around.");
+console.warn("WARN TEST! Watch out for this guy, he'll get ya'!");
