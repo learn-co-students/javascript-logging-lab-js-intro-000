aea9b4d5d50b6834c26acb87d7bba219569d7f36
tonikristian15@gmail.com Christie <tonikristian15@gmail.com>
2018-06-25 08:47:23 +0000

    Done.

---
+++ Javascript lessons/variablelearns.js
@@ -0,0 +1,30 @@
+// declare the variable
+var word
+//assign a value to the variable
+word = 'bird'
+console.log (word) //'bird'
+//assign another value to the variable
+word = 'dog'
+console.log ('word') // 'dog'
+// we can perform variable declaration and assignment on the same line to save space
+var word ='dog'
+// Multiline Variable Assignment
+// let's condense the below code into one line
+var a = 5
+var b = 2
+var c = 3
+var d = 'hello'
+var e = 'goodbye'
+// The above is equivalent to
+var a = 5,
+b =2,
+c = 3,
+d = 'hello',
+e = 'goodbye';
+// CHANGING VALUES
+// Sometimes, information changes — we have to scratch out old notes and scribble in revisions
+// In JavaScript, we can simply reassign the new value to the variable.
+var myNote = "Get milk"
+// later on...
+myNote = "Get cookies"
+// When we change the value of a variable, we do not use var again. We simply use the = to give the variable a new value
