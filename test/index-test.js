bae0d7e06a02c81592a1d68a22ed147c3defab01
orangecrush123 <mbradleylaw@gmail.com>
2018-07-31 02:54:03 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -8,7 +8,7 @@ describe('index', () => {
   const html = '<div></div>'
   const src = path.resolve(__dirname, '..', 'index.js')
 
-  it('calls console.error()', done => {
+  it('calls console.error("HALP")', done => {
   
 
     jsdom.env(html, [src], {
