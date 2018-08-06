90f897ea359d1fadf87775d3ba5d23c4500d8960
Paul Elion <paul.j.elion@gmail.com>
2018-08-06 19:09:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-console.error("There is an error")
-console.log("You've accessed your account")
-console.warn("This is a warning")
+console.error("error");
+console.log("log");
+console.warn("warn");
