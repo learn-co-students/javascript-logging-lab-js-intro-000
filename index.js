c331037737826a2ed491ba67cb28f8e94440734c
helen.marder@wework.com <helen.marder@wework.com>
2018-09-05 02:52:34 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,8 @@
+const expect = require('expect')
+const fs = require('fs')
+const jsdom = require('jsdom')
+const path = require('path')
+
+console.error("HaLP!")
+console.log("You got this!")
+console.warning("DO NOT PASS!")
