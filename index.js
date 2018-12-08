9b8d7bd436cdafc3b74104d05167ee9461d30497
Estibaliz celaya <celayaestibaliz@hotmail.com>
2018-12-08 12:43:50 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,5 @@
+console.error("falla!");
+console.log("hola que hase");
+console.warn("ten cuidadin");
+//me pide que haga cambios para que no se parezca al del profesor, y decido meter un comentario
+console.warn("los comments no son cambios suficientes para que no piensen que son diferentes");
