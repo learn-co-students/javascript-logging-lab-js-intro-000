bb7aa08f255db60dacda0db964a2ce8798855102
Fiona Comerford <fionaco720@gmail.com>
2019-03-08 23:46:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 
-console.error("HALP!")
+console.error("HELP!")
 console.warn("Do not disturb!")
 console.log("Help!")
+
+var word
+word = 'bird'
+console.log(word)
+word = 'dog'
+console.log(word)
+
+console.error("twentys only!")
+console.warn("hundreds left")
+console.log("fresh new bills")
