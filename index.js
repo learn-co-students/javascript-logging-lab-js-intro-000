a71743b7c4b4ae4e7237b6c1962b071953b1f63c
Fiona Comerford <fionaco720@gmail.com>
2019-03-09 00:08:12 +0000

    Done.

--- index.js
+++ index.js
@@ -9,6 +9,25 @@ console.log(word)
 word = 'dog'
 console.log(word)
 
+
 console.error("twentys only!")
 console.warn("hundreds left")
 console.log("fresh new bills")
+
+var word
+word = 'bird'
+console.log(word)
+word = 'dog'
+console.log(word)
+
+var word = 'bird'
+window.word
+
+var a = 5,  b = 2,  c = 3,  d = 'hello',  e = 'goodbye';
+var myNote = "Get Milk"
+myNote = "Get Cookies"
+
+
+
+
+}
