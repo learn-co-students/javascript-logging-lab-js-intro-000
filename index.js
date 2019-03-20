914b6fc159b4da5c64aec78b1c3a0d4bc03e3064
Kelvin Francisco <kelvinfrancisco2100@gmail.com>
2019-03-20 01:08:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 console.error("HALP!");
-console.log("the user should not continue doing what they just did");
+console.log("I would be a logger".);
 console.warn("something broke, and we need to recover");
