4109c02ebb6761cf7209dd14edce07fb570b8c07
Kelvin Francisco <kelvinfrancisco2100@gmail.com>
2019-03-20 00:53:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-console.error("HALP!");
-console.warn(the user should not continue doing what they just did);
-console.log();(something broke, and we need to recover);
+
+console.warn('the user should not continue doing what they just did');
+console.log('something broke, and we need to recover');
