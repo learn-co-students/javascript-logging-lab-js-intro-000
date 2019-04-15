4380b9531829b01b7c3d699e6c3b982efa0d4d76
Brad Ayers <brad@bradayers.com>
2019-04-15 20:23:00 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,4 @@
+console.error("OMG. What is going on?");
+console.log("Thank you. " + "Your ATM transaction is completed.");
+var exampleFormat = "XXXX";
+console.warn("You have entered an incorrect PIN. Please enter in the format: " + exampleFormat);
