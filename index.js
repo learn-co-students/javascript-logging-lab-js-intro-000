019a3e5d7a6aa16fa5dc92d61ed459480837d6e6
Paul Lyles <lyles.paul@gmail.com>
2019-05-04 16:27:14 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,3 @@
+console.error("You caused an ATM machine error. All funds in transaction have been lost. Have a nice day!")
+console.log("Captain's log stardate ... ?")
+console.warn("STOP! you're about to do something that will turn out bad")
