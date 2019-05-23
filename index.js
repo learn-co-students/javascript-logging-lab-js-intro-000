3f280c952ac6ffef540f228dc007c81b3b4b480d
Gabriella Ilena <ilenagabriella@gmail.com>
2019-05-23 14:09:11 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,4 @@
+//Second Lab: Sending messages to users operating an ATM machine
+console.error('Server failed. Please contact the nearest officer for assistance.') //Error message
+console.log('Download our mobile app on Play Store for easier transaction.') //Some information
+console.warn('Entering the wrong PIN 3 times will block your account!') //Warning message
