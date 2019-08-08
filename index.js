d4cf6cb925257d9799d5650f098afb1890be4eec
Padmakanth Rajendram <codse172f-032@student.nibm.lk>
2019-08-08 03:02:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 console.Error("help")
 console.log("hello customer");
-console.log("I would be a logger.");
-console.error("Can't reach server, sorry!") ;
- console.warn("It's taking awhile....");
+console.warn("can't reach server")
+console.Error("server down")
+
+
+ 
