9c36f27f2a02f435edaaeba6bfbdcc8306ff22d9
Riguidix <riguidixrodriguez@gmail.com>
2019-09-16 21:09:20 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,3 @@
+console.error("Oh, Oh, it looks you have an Error, this it's not a mistake");
+console.log("This is a common message, don't be afraid");
+console.warn("This is a warning, you should rethink the way of doing it, cause it may cause troubles");
