c30d4b3b276d17745450cee7e2edb7d27461e7a1
Carly Lord <carly.lord@gmail.com>
2019-10-13 07:42:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-console.error("HALP!")
-console.log("at DESC we log client behaviors")
-console.warn("You can either go to your apartment or go outside")
+console.error('Uh oh,','something is seriously broken...','I\'m scared!')
+console.log('Logging is important for recording client behavior.')
+console.warn('Careful! He\'s off his meds!')
