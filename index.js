32164a04bedc8aeb074e50cd0e51b07615347a8e
GC <manse99@gmail.com>
2019-11-17 16:28:34 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+console.error("HALP!")
+console.log("User is logger")
+console.warn("User is now logged out")
+
+var word
+word = 'bird'
+console.log(word)
+
+word = 'dog'
+console.log(word)
+window.word
+var = 'bird'
+window.word
+var a = 5, b = 2 c = 3 d = 'hello' e = 'goodbye'
+var mynote = 'get milk'
+mynote = 'get cookies'
+hello, everyone!
+var = specialguest = "neil Degrasse Tyson"
+"hello," + specialguest +"!"
