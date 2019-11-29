bcb6f2d1211ec795ea27bc874f1315ea1398f96a
Evan Nelson <evannelson@live.com>
2019-11-29 00:11:32 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,3 @@
+console.error("HALP!")
+console.log("Log is good not back and saves the data in a journal.") // so its pretty much what it means
+console.warn("Warning you have no money to take out!")
