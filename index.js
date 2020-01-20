cb2ca7c1dc24c8ea88dc8f1e730ad5bd5ba710b9
Sean Haverstock <seanhaverstock@gmail.com>
2020-01-20 04:20:20 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,3 @@
+console.error("Still Confused but i can follow instructions")
+console.log("How do I really learn?")
+console.warn("Like, I can type this stuff in, but I don't see the bigger picture")
