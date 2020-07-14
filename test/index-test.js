4684175e91e086321adf4a717413ca5971ea024b
Monique Vassell <momo.vassell@hotmail.com>
2020-07-14 11:56:07 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -8,7 +8,7 @@ describe('index', () => {
   const html = '<div></div>'
   const src = path.resolve(__dirname, '..', 'index.js')
 
-  it('calls console.error()', done => {
+  it ('calls console.error()', done => {
     const spy = expect.spyOn(console, 'error').andCallThrough()
 
     jsdom.env(html, [src], {
@@ -43,4 +43,4 @@ describe('index', () => {
       done()
     })
   })
-})
+  })
