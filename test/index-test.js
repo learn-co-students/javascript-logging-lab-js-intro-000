6f71e3852e65a4f39b3d80c32380559bf0729ad8
legacy-path-0474 <michael.escamilla77@gmail.com>
2020-07-14 22:43:27 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -4,40 +4,40 @@ const jsdom = require('jsdom')
 const path = require('path')
 
 
-describe('index', () => {
+describe('index', () => {console.error("HALP!")
   const html = '<div></div>'
   const src = path.resolve(__dirname, '..', 'index.js')
 
-  it('calls console.error()', done => {
+  it('calls console.error()', done => {console.error("HALP!")
     const spy = expect.spyOn(console, 'error').andCallThrough()
 
     jsdom.env(html, [src], {
       virtualConsole: jsdom.createVirtualConsole().sendTo(console)
-    }, (err, window) => {
+    }, (err, window) => {console.error("HALP!")
       expect(spy).toHaveBeenCalled('expected console.error to have been called')
       console.error.restore()
       done()
     })
   })
 
-  it('calls console.log()', done => {
+  it('calls console.log()', done => {console.log("I would be a logger.")
     const spy = expect.spyOn(console, 'log').andCallThrough()
 
     jsdom.env(html, [src], {
       virtualConsole: jsdom.createVirtualConsole().sendTo(console)
-    }, (err, window) => {
+    }, (err, window) => {console.log("I would be a longer.")
       expect(spy).toHaveBeenCalled('expected console.log to have been called')
       console.log.restore()
       done()
     })
   })
 
-  it('calls console.warn()', done => {
+  it('calls console.warn()', done => {console.warn("I/'m getting the hang of this!")
     const spy = expect.spyOn(console, 'warn').andCallThrough()
 
     jsdom.env(html, [src], {
       virtualConsole: jsdom.createVirtualConsole().sendTo(console)
-    }, (err, window) => {
+    }, (err, window) => {console.warn("I/'m getting the hang of this!")
       expect(spy).toHaveBeenCalled('expected console.warn to have been called')
       console.warn.restore()
       done()
