a75d67ff2249e0375730db229c1ad2ed7cb63b0d
sincere-trainer-5337 <eric.m.lombardo@gmail.com>
2020-08-10 18:03:15 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,8 @@
+// Using console.error() to display an error message
+console.error("please try again")
+
+// Using console.log() to display a message to the user
+console.log("Pin entered correctly")
+
+// Using console.warn() to warn the user something may be wrong
+console.warn("Three more trys until account locks down.")
