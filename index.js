982be2583418a64c3e04a832ffef47c122238e37
wonderful-address-7291 <bilingualedu@gmail.com>
2020-10-10 01:44:04 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,3 @@
+console.error('Do not proceed any further without giving someone a hug')
+console.log('This is the tool everyone needs in their toolbox')
+console.warn('Coding is adictive and may keep you up pass your bedtime')
