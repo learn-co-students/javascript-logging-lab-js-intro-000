39d0a1242fd8080dbee496c1a599ab4b5165f12d
TIm Hackel <tim.hackel24@gmail.com>
2020-10-17 20:07:34 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -8,6 +8,7 @@ describe('index', () => {
   const html = '<div></div>'
   const src = path.resolve(__dirname, '..', 'index.js')
 
+
   it('calls console.error()', done => {
     const spy = expect.spyOn(console, 'error').andCallThrough()
 
@@ -15,7 +16,7 @@ describe('index', () => {
       virtualConsole: jsdom.createVirtualConsole().sendTo(console)
     }, (err, window) => {
       expect(spy).toHaveBeenCalled('expected console.error to have been called')
-      console.error.restore()
+      console.error.restore("Halp!")
       done()
     })
   })
