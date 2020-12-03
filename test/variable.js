cbfc00afefa4b34d69da154268479aab7db43c54
mbula lwanga <18ml253@esisalama.org>
2020-12-03 14:07:21 +0000

    Done.

---
+++ test/variable.js
@@ -0,0 +1,13 @@
+var word="zozer"
+// declare the variable
+var word
+
+// assign a value to the variable
+word = 'bird'
+
+console.log(word) // 'bird'
+
+// assign another value to the variable
+word = 'dog'
+
+console.log(word) // 'dog'
