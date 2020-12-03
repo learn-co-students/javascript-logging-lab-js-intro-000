629c1d656f8e677973bd1733838e05e48aaa730e
mbula lwanga <18ml253@esisalama.org>
2020-12-03 14:32:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 console.error("zozer !")
 console.log("I would be a logger.") // get it?
 console.warn("acomplish my dreams")
+console.error("help!")
+
+console.log('hello!')
+
+console.warn('warning!')
