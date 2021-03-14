461f39ea64023494635393f807af29e5b800686b
Sean Austin Rosenthal <sean.rosenthal@gloanyx.com>
2021-03-14 14:46:05 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,6 @@
+//This is called when their is a dire error in the script.
+console.error('Help');
+//This is called when there is a need to log it down.
+console.log('Write this stuff down');
+//This is called when there is a need to warn someone that something is about to go down.
+console.warn('Something is about to hit the wall');
