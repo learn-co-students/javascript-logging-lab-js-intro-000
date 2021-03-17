c6b2a35d1f81be996960272a5568379c979d2dde
El Magdo <magdoe19@gmail.com>
2021-03-17 10:41:02 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log("Use a very strong password for your ATM account");
+
+console.warn("Your password should contain a mixture of letters, numbers and symbols");
+
+console.error("You shared yuor password!!");
